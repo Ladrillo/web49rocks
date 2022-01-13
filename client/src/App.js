a50eafe3bdabbd182441cca3a12c306cdf44dacd
Ladrillo <gabriel@cabrejas.net>
2022-01-13 20:17:16 +0100

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
+import { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState('web 49 rocks')
+
+  // useEffect(() => {
+  //   fetch('/hello')
+  //     .then(res => res.json())
+  //     .then(d => setData(d))
+  // })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {data}
       </header>
     </div>
   );
